2e12d310490fe48650b9b49e9d282d0d1e953638
AxelMartin69 <axel.tammekand@khk.ee>
2021-09-24 11:17:42 +0300

    Variables

--- app.js
+++ app.js
@@ -1,1 +1,30 @@
-console.log(test)
+/*
+Muutujad
+Vaatame kuidas luua ja kasutada muutujaid
+*/
+// Muutuja mille sisu saab muuta
+let name = "Axel";
+
+name = "Axel-Martin Tammekand";
+
+
+// Muutuja mille nime muuta ei saa
+
+const surname = "Tammekand"
+
+// masiivid
+
+const numbers = [1, 2, 3, 4]
+numbers.push(6)
+
+// objektid
+
+const person = {
+	name: "Kadi",
+	age: "20"
+};
+
+person.age = "21"
+
+console.log(numbers);
+console.log(person);
