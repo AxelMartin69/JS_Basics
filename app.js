799010a4b851edc0348a5614bf71e8a7381918ad
AxelMartin69 <axel.tammekand@khk.ee>
2021-09-24 13:34:18 +0300

    lol1

--- app.js
+++ app.js
@@ -1,27 +1,23 @@
 const name = "Kadi";
 const surname = "Tamm";
 const age = 25;
-const text = "Tere, olen Kadi";
 const tags = "arendus, veeb";
 
 let val;
 
-val = name + " " + surname;
+//template
 
-val = name;
-val += surname
+val = `
+	<ul>
+		<li></li>
+		<li></li>
+		<li></li>
+		<li></li>
+	</ul>	
 
-val = text + ", minu töövaldkond on " + tags;
+`
 
-val = "See on \"Jutumärkides\"";
 
-val = name.toLowerCase();
-
-val = surname.charAt(surname.lenght - 1);
-val = surname.indexOf("a");
-val = surname.slice(0, 2);
-
-val = tags.split(",")
 
 
 console.log(val);
