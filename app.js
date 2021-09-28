7408b3ecbd7bfade4bdfbc63a0b241a637117096
AxelMartin69 <axel.tammekand@khk.ee>
2021-09-28 15:38:44 +0300

    funy eh

--- app.js
+++ app.js
@@ -1,47 +1,9 @@
-// deklareerimine
-function greeting(firstname = 'Kadi', lastname = 'Tamm'){
-	/*
-	if(typeof firstname === 'undefined'){
-		firstname = 'Kadi';
-	}
-	if(typeof lastname === 'undefined'){
-		lastname = 'Tamm';
-	}
-	*/
-	return 'Tere, ' + firstname + ' ' + lastname;
-}
-// kasutamine
-greet = greeting('Anna', 'Karutina');
-console.log(greet);
+let val;
 
-// f-on defineeritud nagu const
-const square = function(number = 3){
-	return Math.pow(number, 2);
-};
-console.log(square());
+val = document;
+val = document.all;
+val = document.all[4];
 
-// f-on, mis kohe töötab koodis
-(function(){
-	console.log('function is ran');	
-})();
 
-(function(name){
-	console.log('Tere, ' + name);
-})('Anna');
 
-// f-oni objekti atribuutidena
-const todoList = {
-	add: function(task){
-		console.log(task + ' is added');
-	},
-	edit: function(oldtask, newtask){
-		console.log(`${oldtask} is changed to ${newtask}`);
-	},
-	delete: function(task){
-		console.log(task + ' is deleted');
-	}
-};
-
-todoList.add('Study JS');
-todoList.edit('Study JS', 'Study TypeScript');
-todoList.delete('Study JS');
+console.log(val)
