909beccf7d05fba5d0e87701c61bb98cb98c97af
AxelMartin69 <axel.tammekand@khk.ee>
2021-09-28 14:33:53 +0300

    functions

--- app.js
+++ app.js
@@ -1,20 +1,13 @@
-// for tsükkel
-//for(let i = 0; i < 10; i++){
-	//if(i % 2 === 0){
-	//	console.log(i)
-	//}
-//}
-/*
-for(let i = 0; i < 10, i++){
-	if(i === 5) break;
-	if(i === 5) continue;
-	console.log(i);
+// deklareerimine
+
+function greeting(firstname, lastname){
+	if(typeof firstname === 'undefined'){
+		firstname = 'Axel'
+	}
+	if(typeof lastname === 'undefined'){
+		lastname = 'Tammekand'
+	}
+	return "Tere, " + firstname + ' ' + lastname;
 }
 
-
-let j = 0
-do{
-	console.log(j)
-	j++;
-} while(j < 10);
-*/
+console.log(greeting())
