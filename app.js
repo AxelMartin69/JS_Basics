d52172d4424e4d714643e1e9fd0e13de0e49dbd7
AxelMartin69 <axel.tammekand@khk.ee>
2021-10-01 13:06:39 +0300

    jes

--- app.js
+++ app.js
@@ -1,8 +1,19 @@
 let val;
 
-val = document.getElementById("task-title");
+val = document.getElementById('task-title');
 
-document.getElementById("task-title").style.background = ""
-document.getElementById("task-title").textContent = "My Tasks"
+document.getElementById('task-title').style.background = '#333';
+document.getElementById('task-title').style.color = '#fff';
+document.getElementById('task-title').style.padding = '5px';
 
-console.log(val)
+document.getElementById('task-title').textContent = 'My Tasks';
+document.getElementById('task-title').innerText = 'My Favorite Tasks';
+document.getElementById('task-title').innerHTML = '<span style="color: orange;">My Tasks</span>'
+
+val = document.querySelector('h4');
+val = document.querySelector('#task-title');
+val = document.querySelector('.collection-item');
+val = document.querySelector('.collection-item:nth-child(even)');
+val = document.querySelector('.collection-item:nth-child(odd)');
+
+console.log(val);
